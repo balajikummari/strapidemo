8545e96c0da6ca2f3fcbee2b7d563a094374ce0a
Balaji Kummari <balajievlo@gmail.com>
2020-09-12 15:23:24 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '389440b43b40249f3ee72c08734fc16d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'abe6abe4-01c8-4a6a-9480-b46800e955e1'
+};
